9b698f408c14cad69266690c262c854991e570db
Richard <richardhayes777@gmail.com>
2020-10-12 08:03:43 +0100

    failed attempt to use a pitch bend

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import mido
 import pygame
 from pygame import midi
@@ -13,5 +15,12 @@ def play_note(channel=0, note=50, velocity=50):
     output.send(mido.Message("note_on", channel=channel, note=note, velocity=velocity))
 
 
+def pitch_bend(value, channel=0):
+    output.send(mido.Message('pitchwheel', pitch=value, channel=channel))
+
+
 if __name__ == "__main__":
     play_note()
+    time.sleep(1)
+    play_note()
+    pitch_bend(100)
