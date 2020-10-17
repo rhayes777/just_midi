d33b247c2b84fce770f51d1598f5988ec6900c70
Richard <richardhayes777@gmail.com>
2020-10-17 17:08:32 +0100

    packaged input in a class

--- just_midi/main.py
+++ just_midi/main.py
@@ -15,46 +15,47 @@ def pitch_bend(value, channel=0):
     output.send(mido.Message('pitchwheel', pitch=value, channel=channel))
 
 
-note_map = {
-    pygame.K_a: 65,
-    pygame.K_w: 66,
-    pygame.K_s: 67,
-    pygame.K_e: 68,
-    pygame.K_d: 69,
-    pygame.K_r: 70,
-    pygame.K_f: 71,
-    pygame.K_g: 72,
-    pygame.K_y: 73,
-    pygame.K_h: 74,
-    pygame.K_u: 75,
-    pygame.K_j: 76,
-    pygame.K_k: 77,
-    pygame.K_o: 78,
-    pygame.K_l: 79,
-    pygame.K_p: 80,
-    pygame.K_SEMICOLON: 81,
-    pygame.K_LEFTBRACKET: 82,
-    pygame.K_QUOTE: 83,
-    pygame.K_BACKSLASH: 84
-}
+class StopPlay(Exception):
+    pass
 
-type_map = {
-    pygame.KEYDOWN: "note_on",
-    pygame.KEYUP: "note_off"
-}
 
-if __name__ == "__main__":
-    velocity = 50
-    channel = 0
-    play = True
-    while play:
-        clock.tick(40)
+class Input:
+    note_map = {
+        pygame.K_a: 65,
+        pygame.K_w: 66,
+        pygame.K_s: 67,
+        pygame.K_e: 68,
+        pygame.K_d: 69,
+        pygame.K_r: 70,
+        pygame.K_f: 71,
+        pygame.K_g: 72,
+        pygame.K_y: 73,
+        pygame.K_h: 74,
+        pygame.K_u: 75,
+        pygame.K_j: 76,
+        pygame.K_k: 77,
+        pygame.K_o: 78,
+        pygame.K_l: 79,
+        pygame.K_p: 80,
+        pygame.K_SEMICOLON: 81,
+        pygame.K_LEFTBRACKET: 82,
+        pygame.K_QUOTE: 83,
+        pygame.K_BACKSLASH: 84
+    }
+
+    type_map = {
+        pygame.KEYDOWN: "note_on",
+        pygame.KEYUP: "note_off"
+    }
+
+    def __call__(self):
+        messages = list()
         events = pygame.event.get()
         for event in events:
-            if event.type in type_map and event.key in note_map:
-                message_type = type_map[event.type]
-                note = note_map[event.key]
-                output.send(
+            if event.type in self.type_map and event.key in self.note_map:
+                message_type = self.type_map[event.type]
+                note = self.note_map[event.key]
+                messages.append(
                     mido.Message(
                         message_type,
                         channel=channel,
@@ -63,4 +64,19 @@ if __name__ == "__main__":
                     )
                 )
             if event.key == pygame.K_ESCAPE:
-                play = False
+                raise StopPlay()
+        return messages
+
+
+if __name__ == "__main__":
+    velocity = 50
+    channel = 0
+    play = True
+    input_ = Input()
+    while play:
+        clock.tick(40)
+        try:
+            for message in input_():
+                output.send(message)
+        except StopPlay:
+            play = False
