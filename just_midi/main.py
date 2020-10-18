cb0e83368f0c887b46eb4ff505185f9fca4bd3ac
Richard <richardhayes777@gmail.com>
2020-10-18 10:53:34 +0100

    working

--- just_midi/main.py
+++ just_midi/main.py
@@ -2,6 +2,7 @@ import mido
 import pygame
 from pygame import midi
 
+from just_midi import interval as i
 from just_midi.control import StopPlay, Input
 
 clock = pygame.time.Clock()
@@ -11,6 +12,47 @@ midi.init()
 mido.set_backend("mido.backends.pygame")
 
 output = mido.open_output()
+
+PITCHWHEEL_SEMITONE = mido.MAX_PITCHWHEEL / 2
+
+
+class JustOutput:
+    def __init__(self, output_):
+        self._state = None
+        self._output = output_
+
+    def send(self, message):
+        note = message.note
+        if self._state is None:
+            self._state = i.State(
+                frequency=i.frequency_for_note_number(
+                    note
+                ),
+                note=note
+            )
+        frequency = self._state(note)
+        note_number = i.note_number_for_frequency(
+            frequency
+        )
+        remainder = note_number % 1
+        note = int(note_number)
+        bend = int(
+            remainder * PITCHWHEEL_SEMITONE
+        )
+        self._output.send(
+            mido.Message(
+                'pitchwheel',
+                pitch=bend,
+                channel=message.channel
+            )
+        )
+        self._output.send(
+            mido.Message(
+                message.type,
+                note=note,
+                velocity=message.velocity
+            )
+        )
 
 
 def pitch_bend(value, channel=0):
@@ -38,6 +80,6 @@ class Runner:
 if __name__ == "__main__":
     runner = Runner(
         input_=Input(),
-        output_=output
+        output_=JustOutput(output)
     )
     runner.run()
